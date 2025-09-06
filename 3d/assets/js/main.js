fbcc851c74b47e353143679410b10af04ec2709b
Morakinyo Kolade <72721600+morakinyo-kolade@users.noreply.github.com>
2025-09-06 17:05:28 +0100

    main.js

--- 3d/assets/js/main.js
+++ 3d/assets/js/main.js
@@ -1,32 +1,13 @@
-/*
-	Photon by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+// Custom cursor follow
+const cursor = document.querySelector('.cursor');
 
-(function($) {
+document.addEventListener('mousemove', e => {
+  cursor.style.top = e.pageY + 'px';
+  cursor.style.left = e.pageX + 'px';
+});
 
-	var	$window = $(window),
-		$body = $('body');
-
-	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1141px',  '1680px' ],
-			large:    [ '981px',   '1140px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ '321px',   '480px'  ],
-			xxsmall:  [ null,      '320px'  ]
-		});
-
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
-
-	// Scrolly.
-		$('.scrolly').scrolly();
-
-})(jQuery);
+// Cursor expand on hover
+document.querySelectorAll('.glass-item, .navbar span').forEach(item => {
+  item.addEventListener('mouseenter', () => cursor.classList.add('cursor-grow'));
+  item.addEventListener('mouseleave', () => cursor.classList.remove('cursor-grow'));
+});
