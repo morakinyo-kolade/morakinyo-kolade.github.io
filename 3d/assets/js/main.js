ffa1798f0472450b525d744bae84ceeae3234e48
Morakinyo Kolade <72721600+morakinyo-kolade@users.noreply.github.com>
2025-09-06 19:37:05 +0100

    main.js

--- 3d/assets/js/main.js
+++ 3d/assets/js/main.js
@@ -1,16 +1,1 @@
-const cursor = document.querySelector('.custom-cursor');
 
-document.addEventListener('mousemove', (e) => {
-  cursor.style.left = e.clientX + 'px';
-  cursor.style.top = e.clientY + 'px';
-});
-
-const hoverElements = document.querySelectorAll('.glass-item, .navbar span');
-hoverElements.forEach(el => {
-  el.addEventListener('mouseenter', () => {
-    cursor.classList.add('cursor-grow');
-  });
-  el.addEventListener('mouseleave', () => {
-    cursor.classList.remove('cursor-grow');
-  });
-});
